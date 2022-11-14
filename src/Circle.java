799fc1942f486c5b54ba6caa4081d182de9b0e0d
Kramil <kamil21mrdc@gmail.com>
2022-11-14 12:52:28 +0100

    Amélioration partie graphique de la grille

--- src/Circle.java
+++ src/Circle.java
@@ -14,16 +14,18 @@ public class Circle extends JPanel {
      * @param radius the radius of the circle
      */
     public Circle(int radius, Jeu jeu) {
-        this.setPreferredSize(new Dimension(700, 700));
+        this.setPreferredSize(new Dimension(600, 520));
         this.radius = radius;
         this.jeu = jeu;
         this.colNum = this.jeu.grille[0].length;
         this.rowNum = this.jeu.grille.length;
+        this.setBackground(Color.BLUE);
 
     }
 
     public int  getcolonne(){
         return this.colonne;
+
     }
 
     /**
@@ -41,25 +43,35 @@ public class Circle extends JPanel {
         if (this.isOpaque()) {
             this.brush.setColor(this.getBackground());
             this.brush.fillRect(0, 0, this.getWidth(), this.getHeight());
+
         }
 
         for (int r = 0 ; r < rowNum; r++) {
             for (int c = 0 ; c < colNum; c++) {
-                if (jeu.grille[r][c] == 0) {
-                    this.colonne = c;
-                    this.brush.setColor(Color.LIGHT_GRAY);
-                    this.brush.fillOval((c * 75) + 50, (r * 75) + 50, this.radius, this.radius);
-                } else if (jeu.grille[r][c] == 1) {
-                    this.brush.setColor(Color.RED);
-                    this.brush.fillOval((c * 75) + 50, (r * 75) + 50, this.radius, this.radius);
-                } else if (jeu.grille[r][c] == 2) {
-                    this.brush.setColor(Color.YELLOW);
-                    this.brush.fillOval((c * 75) + 50, (r * 75) + 50, this.radius, this.radius);
-                } else if (jeu.grille[r][c] == 3) {
-                    this.brush.setColor(Color.PINK);
-                    this.brush.fillOval((c * 75) + 50, (r * 75) + 50, this.radius, this.radius);
+                switch (jeu.grille[r][c]) {
+                    case 0:
+                        this.brush.setColor(Color.WHITE);
+                        break;
+
+                    case 1:
+                        this.brush.setColor(Color.RED);
+                        break;
+
+                    case 2:
+                        this.brush.setColor(Color.YELLOW);
+                        break;
+
+                    case 3:
+                        this.brush.setColor(Color.GREEN);
+                        break;
+
                 }
+                this.brush.fillOval((c * 75) + 50, (r * 75) + 50, this.radius, this.radius);
+                this.brush.setColor(Color.DARK_GRAY);
+                this.brush.drawOval((c * 75) + 50, (r * 75) + 50, this.radius, this.radius);
+
             }
+
         }
 
     }
