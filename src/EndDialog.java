87a28e7fe589d1da1145bbf123465ffb612d12e6
KeaganZX <yanisbouarroudj148@gmail.com>
2022-11-20 23:41:13 +0100

    scoreboard

--- src/EndDialog.java
+++ src/EndDialog.java
@@ -13,8 +13,6 @@ public class EndDialog extends JDialog {
         this.setSize(new Dimension(300, 300));
         this.setLocation(this.window.getX()/2, this.window.getY()/2);
         System.out.println("test"+this.jeu.getScoreboard());
-        JLabel label = new JLabel("Fin de la partie"+this.jeu.getScoreboard());
-        this.add(label);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
     }
@@ -24,8 +22,17 @@ public class EndDialog extends JDialog {
         this.setSize(new Dimension(300, 300));
         this.setLocation(this.window.getX()/2, this.window.getY()/2);
         System.out.println("test"+this.jeu.getScoreboard());
-        JLabel label = new JLabel("Fin de la partie"+this.jeu.getScoreboard());
+        JLabel label = new JLabel("Fin de la partie");
+        JLabel score = new JLabel("Scoreboard : ");
+        JLabel gagnant1 = new JLabel("Gagnant 1 : Joueur "+ this.jeu.getScoreboard().get(0));
+        JLabel gagnant2 = new JLabel("Gagnant 2 : Joueur "+ this.jeu.getScoreboard().get(1));
+
+        this.setLayout(new GridLayout(4,0));
+
         this.add(label);
+        this.add(score);
+        this.add(gagnant1);
+        this.add(gagnant2);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.repaint();
